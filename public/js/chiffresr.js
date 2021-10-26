0c732d98fba7f3beea4f1c8b7d8be6293057022d
dawiteu <dawid@tararuj.be>
2021-10-26 17:47:19 +0200

    automatic array init

--- public/js/chiffresr.js
+++ public/js/chiffresr.js
@@ -1,23 +1,20 @@
-const numbersProperty = {
-    1: "I",
-    2: "II", 
-    3: "III",
-    4: "IV",
-    5: "V",
-    6: "VI", 
-    7: "VII",
-    8: "VIII",
-    9: "IX", 
-    10: "X", 
-        40: "XL", 
-    50: "L", 
-        90: "XC", 
-    100: "C",
-        140: "CD", 
-        150: "D", 
-        900: "CM", 
-    1000: "M"
-}
+const numbers = ["I", "V", "X", "L", "C", "D", "M"]; 
+const arraysNumbers = []; 
+
+
+const groupNumbers = numbers.forEach((number, index) => {
+    let newTab = []; 
+    newTab[0] = ""; 
+    newTab[1] = number; 
+    newTab[2] = number+number; 
+    newTab[3] = number+number+number; 
+    newTab[4] = number+numbers[numbers.indexOf(number)+1];
+    arraysNumbers.push(newTab); 
+});
+
+console.log(groupNumbers, arraysNumbers); 
+
+
 //console.log(numbersProperty[1]); 
 
 const convertNumber = (number) => {
